d36248d408ce5d693d5f0eecc30ab8745c824d6f
J Eckert <sargonas@gmail.com>
2014-09-16 17:28:30 -0700

    adding copy to clipboard
    added copy co clipboard function, and fixed broken css

--- codegen.php
+++ codegen.php
@@ -13,7 +13,9 @@ $url = sprintf($url_format, $_GET['map'], $_GET['type'], $_GET['teamsize'], $_GE
   <head>
     <meta charset="utf-8">
     <link rel="stylesheet" href="style.css"> 
-    <title>Tournament code generated</title>
+    <title>Denewb TCode</title>
+	<script type="text/javascript" src="jquery-1.8.0.min.js"></script> 
+	<script src="jquery.zclip.js"></script>
     <style>
         pre {
             width: 400px;
@@ -22,11 +24,21 @@ $url = sprintf($url_format, $_GET['map'], $_GET['type'], $_GET['teamsize'], $_GE
             word-wrap: break-word;
         }
     </style>
+	<script>
+		$(document).ready(function(){
+			$("a#copy-link").zclip({
+				path:"ZeroClipboard.swf",
+				copy:function(){return $("input#copy-box").val();}
+			});
+		});
+	</script>
   </head>
   <body>
     <center><img src="banner.jpg" alt="image description" width="500"></center>
     <p>
-    <h1 class="bootstrap-frm" align="center">Tournament code generated</h1>
-    <pre class="bootstrap-frm"><?php echo $url ?></pre>
+    <h1 class="form" align="center">Tournament code generated</h1>
+    <pre class="form"><?php echo $url ?></pre>
+	<input type="text" id="copy-box" value="testtext" />
+	<a href="#copy" id="copy-link">Copy Now</a>
   </body>
 </html>
